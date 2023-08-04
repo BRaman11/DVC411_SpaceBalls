2426acef412b5bc6c9682a4478dff424402886a2
Parkersthetics <jparker4@myjmc.edu.au>
2023-08-04 12:06:27 +1000

    use this bitch

---
+++ Space Balls/Assets/Scripts/ScoreArea.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour {
+    public GameObject effectObject;
+
+    void Start () {
+        effectObject.SetActive (false);
+    }
+
+    void OnTriggerEnter (Collider otherCollider) {
+        GameObject ball = otherCollider.gameObject;
+        if (ball.name=="Ball") {
+            effectObject.SetActive (true);
+        }
+    }
+}
